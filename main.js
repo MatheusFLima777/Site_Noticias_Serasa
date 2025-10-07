4a86c8f11636898b0a2a6ef2baa4bca823cd6151
Matheus.Lima <limamatheus0010@gmail.com>
2025-10-07 03:41:46 -0300

    Feat: Adição da pagina Lançamentos

--- main.js
+++
@@ -1,8 +0,0 @@
-const iconeMenu = document.querySelector('.icone-menu');
-const menuAtivo = document.querySelector('.menu-mobile');
-
-iconeMenu.addEventListener('click', () => {
-  menuAtivo.classList.toggle('menu-ativo');
-});
-
-
